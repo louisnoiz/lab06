a09f25ce55585aac62ecae96583cd93055069ab9
louisnoiz <73683461+louisnoiz@users.noreply.github.com>
2020-11-20 14:58:50 +0900

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,16 +1,16 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'phufarpsw.mysql.database.azure.com', 'phufarpsw@phufarpsw', 'EMLbmk28', 'itflab', 3306);
+mysqli_real_connect($conn, 'louis.mysql.database.azure.com', 'louisnoiz@louis', 'Louis250445', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
+$name = $_POST['Name'];
+$comment = $_POST['Comment'];
+$link = $_POST['Link'];
 
 
 $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
